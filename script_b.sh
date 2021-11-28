99ead1fd7f94ce4897558e5ed3bef7d0c8430fa6
andifaridz <andifaridz@gmail.com>
2021-11-28 19:00:32 +0700

    new file script script_b.sh

---
+++ script_b.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+printf "\n"
+printf "Ini adalah sebuah program dari script bernama script_b.sh untuk menampilkan environment variable PATH. \n"
+printf "Tanggal hari ini: `date`, minggu ke - `date +"%V"`. \n\n"
+printf "\n"
+
+printf "Berikut adalah environment variable PATH: \n"
+echo $PATH
+printf "\n"
